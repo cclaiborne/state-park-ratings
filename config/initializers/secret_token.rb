3a5f0bbb5477b92e5cc6c77328a8eb826658c632
cclaiborne <claiborne206@gmail.com>
2014-02-03 12:17:00 -0800

    add project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Statepark::Application.config.secret_key_base = '946ffb37bdeabc00139130185c57156a5d2ecb239bbac3bf327fc77e0e527f57b55e520a99671ea201f0427552f42bea523e0198bd6b5f07120e740bffa137b8'
