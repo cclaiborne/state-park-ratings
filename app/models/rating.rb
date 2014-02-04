fca5ae77f0b473b30a0ceafcef7ec61e26d4904e
sealocal <local.mat@gmail.com>
2014-02-04 13:34:15 -0800

    add seeds for dev

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,4 @@
 class Rating < ActiveRecord::Base
-  belongs_to: :user
-  belongs_to: :campsite
+  belongs_to :user
+  belongs_to :campsite
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+    { id: 1, name: 'Happy Camper', email: 'happy_camper@example.com' },
+    { id: 2, name: 'Sad Camper', email: 'sad_camper@example.com' }
+  ])
+
+campsites = Campsite.create([
+    { id: 11, name: "Serenity Forrest Campground", description: "Quite and peaceful campground on the water.", location: "Hoh, WA" },
+    { id: 12, name: "Swampy Pastures Campground", description: "Remote campground with distinguished aromas.", location: "Portland, OR" },
+  ])
+
+ratings = Rating.create([
+    { id: 10, stars: 5, comment: "What a great campsit.", user_id: 1, campsite_id: 11 },
+    { id: 20, stars: 1, comment: "This place smells.", user_id: 2, campsite_id: 12}
+  ])
