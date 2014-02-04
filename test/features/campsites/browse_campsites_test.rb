1416310f98c48aed60b9b936d8222a99eaf0a074
sealocal <local.mat@gmail.com>
2014-02-03 19:27:52 -0800

    add campsite browsing test with fixtures

--- test/features/campsites/browse_campsites_test.rb
+++ test/features/campsites/browse_campsites_test.rb
@@ -3,15 +3,33 @@ require "test_helper"
 feature "BrowseCampsites" do
   scenario "As a visitor I want to browse campsites so
   that I can find the most recently rated campsites" do
-    #given I visit the campsite index
+    #given the campsite index page
     visit root_path
     #when I browse campsites
-    #page.will_have_content "Goobye All!"
-    #page.assert_selector('table tr')
     page.must_have_selector('table tr', :count => Campsite.all.count + 1)
     #then I find the highest rated campsites
     page.must_have_selector('tbody td[1]')
     @recentcampsiteid = Ratings.order(:created_at).last.campsite_id
     page.find('tbody td[1]').text.must_include(@recentcampsiteid)
+  end
+
+  scnenario "As a visitor I want to see a map of all the campsite locations
+  so that I know where it is located." do
+    #given a visitor
+    #when I browse the home page
+    visit root_path
+    #then I will see a map on the home page
+    page.must_have_selector('#map')
+  end
+
+  scnenario "As a visitor I want to see a map of the campsite location
+  so that I know where it is located." do
+    #given a campsite index page
+    visit root_path
+    #when I browse to the first campsite's show page
+    page.find('a', text: "Show")[0].click
+    #then I will see the campsite's name and a map of its location
+    page.text.must_include(campsites(:good_campsite).name)
+    page.must_have_selector('#map')
   end
 end
