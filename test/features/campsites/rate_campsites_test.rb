780bc1cfba46a3d5674a076b0f25a77dcc7ff73e
sealocal <local.mat@gmail.com>
2014-02-03 19:03:48 -0800

    add fixture name to campsite_path

--- test/features/campsites/rate_campsites_test.rb
+++ test/features/campsites/rate_campsites_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
 feature "RateCampsites" do
-  scenario "As a visitor I want to rate campsites so that
-  others may find a well ranked campsite." do
+  scenario "As a visitor I want to rate campsites so
+  that others may find a well ranked campsite." do
     #given I visit the campsite show page
-    visit campsite_path
+    visit campsite_path(campsites(:good_campsite))
     #when I  a campsite with a number and comment
     fill_in 'Rating', :with => '5'
     fill_in 'Comment', :with => 'This is a great campsite!'
